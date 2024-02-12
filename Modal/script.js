a70db6606fd6c5c0691e5cae18ed5834996c12ef
Leon Berkemeier <leonberkemeier@gmail.com>
2024-02-12 12:52:37 +0100

    funcRmAndAddModal

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,13 @@
 // alert('1')
+
+let modal = document.getElementById('modal')
+
+function openModal(){
+    modal.classList.remove('modal-hidden');
+    // alert('opening modal');
+}
+
+function removeModal() {
+    modal.classList.add('modal-hidden');
+    // alert('removing modal');
+}
--- djangocrud/crudapp/static/js/script.js
+++ djangocrud/crudapp/static/js/script.js
@@ -1,1 +1,13 @@
 // alert('1')
+
+const modal=document.getElementById('quest')
+
+function openModal(){
+    modal.classList.remove('modal-hidden');
+    // alert('opening modal');
+}
+
+function removeModal() {
+    modal.classList.add('modal-hidden');
+    // alert('removing modal');
+}
